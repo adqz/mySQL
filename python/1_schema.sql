3c599151f79cb763896ad3c235d049ae718c0045
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-06-03 15:01:43 -0700

    created table and added data

---
+++ python/1_schema.sql
@@ -0,0 +1,7 @@
+USE mySqlPython;
+CREATE TABLE taula (
+    time INT(13),
+    username VARCHAR(20),
+    tweet VARCHAR(200)
+);
+DESCRIBE taula;
---
+++ python/2_insert_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO taula VALUES(1385419969, "adnan", "welcome to sample tutorial");
+INSERT INTO taula VALUES(1385419970, "adnan", "nothing beats surfing in san diego summer!");
+INSERT INTO taula VALUES(1385419971, "adnan", "@BillGates, eat shit");

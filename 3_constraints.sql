e8951545c86106cf09ed264520eb1afa885f25d3
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-06-02 18:19:00 -0700

    constrained 2 attributes and tested it

---
+++ 3_constraints.sql
@@ -0,0 +1,20 @@
+DROP TABLE student;
+CREATE TABLE student (
+    student_id INT,
+    name VARCHAR(50) NOT NULL,
+    major VARCHAR(50) UNIQUE,
+    PRIMARY KEY (student_id)
+);
+
+DESCRIBE student;
+
+-- ALTER TABLE student ADD gpa DECIMAL(3,2);
+-- ALTER TABLE student DROP column gpa;
+
+INSERT INTO student VALUES(1, 'Jack', 'Biology');
+INSERT INTO student VALUES(2, 'Kate', 'Sociology');
+INSERT INTO student(student_id, name, major) VALUES(3, NULL, 'Chemistry');
+INSERT INTO student VALUES(4, 'Jack', 'Biology');
+INSERT INTO student VALUES(5, 'Mike', 'Computer Science');
+
+SELECT * FROM student;

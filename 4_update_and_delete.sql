10083e747ddb3508aa4bed2e1ace1e9b0baddd08
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-06-02 18:31:18 -0700

    added update statements with logic involved

--- 4_update_and_delete.sql
+++ 4_update_and_delete.sql
@@ -11,3 +11,11 @@ WHERE major = 'Computer Science';
 UPDATE student
 SET major = 'Comp Sci'
 WHERE student_id = 4;
+
+UPDATE student
+SET major = 'Biochemistry'
+WHERE major = 'Bio' OR major = 'Chemistry';
+
+UPDATE student
+SET name ='Tom', major='undecided'
+WHERE student_id = 1;

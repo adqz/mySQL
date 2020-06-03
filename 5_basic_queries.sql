ff3ab4244f574e539473233c877f6a3834d76852
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-06-02 18:41:42 -0700

    limited and filtered data

--- 5_basic_queries.sql
+++ 5_basic_queries.sql
@@ -4,3 +4,12 @@ FROM student;
 SELECT *
 FROM student
 ORDER BY major, student_id DESC;
+
+SELECT *
+FROM student
+ORDER BY student_id DESC
+LIMIT 2;
+
+SELECT *
+FROM student
+WHERE major = 'Biology';

9058e24e6921b8eb62f10ae6607949066162338f
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-06-02 18:28:32 -0700

    added complex update statements

--- 4_update_and_delete.sql
+++ 4_update_and_delete.sql
@@ -3,3 +3,11 @@ SELECT * FROM student;
 UPDATE student
 SET major = 'Bio'
 WHERE major = 'Biology';
+
+UPDATE student
+SET major = 'Comp Sci'
+WHERE major = 'Computer Science';
+
+UPDATE student
+SET major = 'Comp Sci'
+WHERE student_id = 4;

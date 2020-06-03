c7f496f62796371a06f56659d885cb8155fda235
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2020-06-02 18:22:58 -0700

    implemented auto increment on primary key

--- 3_constraints.sql
+++ 3_constraints.sql
@@ -1,8 +1,8 @@
 DROP TABLE student;
 CREATE TABLE student (
-    student_id INT,
-    name VARCHAR(50) NOT NULL,
-    major VARCHAR(50) UNIQUE,
+    student_id INT AUTO_INCREMENT,
+    name VARCHAR(50),
+    major VARCHAR(50) DEFAULT 'undecided',
     PRIMARY KEY (student_id)
 );
 
@@ -11,10 +11,10 @@ DESCRIBE student;
 -- ALTER TABLE student ADD gpa DECIMAL(3,2);
 -- ALTER TABLE student DROP column gpa;
 
-INSERT INTO student VALUES(1, 'Jack', 'Biology');
-INSERT INTO student VALUES(2, 'Kate', 'Sociology');
-INSERT INTO student(student_id, name, major) VALUES(3, 'Claire', 'Chemistry');
-INSERT INTO student VALUES(4, 'Jack', 'Biology');
-INSERT INTO student VALUES(5, 'Mike', 'Computer Science');
+INSERT INTO student(name, major) VALUES('Jack', 'Biology');
+INSERT INTO student(name, major) VALUES('Kate', 'Sociology');
+INSERT INTO student(name, major) VALUES('Claire', 'Chemistry');
+INSERT INTO student(name, major) VALUES('Jack', 'Biology');
+INSERT INTO student(name, major) VALUES('Mike', 'Computer Science');
 
 SELECT * FROM student;
